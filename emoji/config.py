f625978fa234b618b91f8bc78b9e5a51265e83e2
Nicholas Beninato <beninato8@gmail.com>
2020-10-03 01:07:50 -0700

    config

---
+++ emoji/config.py
@@ -0,0 +1,12 @@
+# config file
+
+# user defined name:emoji pairs
+extra = {u':first_place_medal:': u'\U0001F947',
+    u':second_place_medal:': u'\U0001F948',
+    u':third_place_medal:': u'\U0001F949'}
+
+# name of collection
+collection_name = 'emoji'
+
+# default to always autoexpand snippets
+dontautoexpand = False
