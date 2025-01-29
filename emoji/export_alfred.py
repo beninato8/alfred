8c72bf0c0ea66beeb5a7757ba8f9ef6568c0d756
Nicholas Beninato <beninato8@gmail.com>
2025-01-28 23:39:34 -0800

    add cldr annotations

--- emoji/export_alfred.py
+++ emoji/export_alfred.py
@@ -5,6 +5,7 @@ from emoji import EMOJI_DATA
 import json
 import os
 from pprint import pprint
+from collections import Counter
 
 emojis = dict()
 for e, data in EMOJI_DATA.items():
@@ -16,7 +17,24 @@ for e, data in EMOJI_DATA.items():
             emojis[f'{n[:-1]}_{name[1:]}'] = e
     emojis[data['en']] = e
 
-emojis ={**emojis, **extra} # emoji library + user config
+# from https://github.com/unicode-org/cldr-json/blob/main/cldr-json/cldr-annotations-full/annotations/en/annotations.json
+with open('annotations.json', 'r') as f:
+    cldr_data = json.load(f)['annotations']['annotations']
+
+cldr = dict()
+stats = []
+ignore = {'face', 'animal', 'arrow', 'button', 'food', 'person', 'hand', 'right', 'mathematics'}
+for i, (e, data) in enumerate(cldr_data.items()):
+    if len(e) > 1 or ord(e) < 124:
+        continue
+    name = '_'.join(' '.join(data['tts']).split(' '))
+    for alias in [f':{n}_{name}:' for n in set(data['default']) - ignore]:
+        cldr[alias] = e
+    for d in data['default']:
+        stats.append(d)
+
+# emoji library + user config + unicode annotations
+emojis ={**emojis, **extra, **cldr}
 
 def get_uuid(s):
     """use hash to keep uuid constant when re-running"""
@@ -37,6 +55,9 @@ def make_snippet(name, uni_char):
 
 home = os.path.expanduser('~') # home directory
 path = f'{home}/.config/alfred/Alfred.alfredpreferences/snippets/{collection_name}'
+
+if not os.path.exists(path):
+    os.makedirs(path)
 
 for name, uni_char in emojis.items(): # iterate over all name:emoji pairs
     snippet = make_snippet(name, uni_char)
